19507be5f8b7dbac37cd13d748a57f5ff038d806
Timothy <timothychang@tutanota.com>
2020-02-08 09:03:22 -0800

    Add logout button to head and fix style on album element

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { clearAuth } from './actions/auth';
 import './Header.css';
+import logout from './images/logout.svg';
 
 const Header = (props) => {
     return (
         <header>
-            <h1>Albumify</h1>
-            <button onClick={() => props.dispatch(clearAuth())}>Logout</button>
+            <div className="header-items">
+                <h1>Albumify</h1>
+                <img alt={logout} onClick={() => props.dispatch(clearAuth())} src={logout}/>
+            </div>
         </header>
     );
 };
