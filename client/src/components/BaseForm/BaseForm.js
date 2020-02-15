3c45ea77f03a6db917765c1315c79b39e82c072c
Timothy <timothychang@tutanota.com>
2020-02-15 00:34:12 -0800

    Rewrite BaseForm component

--- client/src/components/BaseForm/BaseForm.js
+++ client/src/components/BaseForm/BaseForm.js
@@ -4,9 +4,9 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './Form.css';
 
-export default class extends React.Component {
-	onSubmit = (values) => {
-		return this.props.submitRequest(values)
+export default (props) => {
+	const onSubmit = (values) => {
+		return props.submitRequest(values)
 			.catch(e => {
 				toast.error(e.errors._error, {
 					className: 'toast'
@@ -14,24 +14,20 @@ export default class extends React.Component {
 			});
 	};
 
-	render = () => {
-		const { buttonText } = this.props;
-		const { handleSubmit, pristine, submitting } = this.props;
-		const disabled = pristine || submitting;
+	const { buttonText, handleSubmit, pristine, submitting } = props;
+	const disabled = pristine || submitting;
 
-		return (
-			<form onSubmit={handleSubmit(values => this.onSubmit(values))}>
-				<div className='input'>
-					<label htmlFor='username'>Username</label>
-					<Field component='input' disabled={submitting} name='username' type='text'/>
-				</div>
-				<div className='input'>
-					<label htmlFor='password'>Password</label>
-					<Field component='input' disabled={submitting} name='password' type='text'/>
-				</div>
-				<button disabled={disabled} type="submit">{buttonText}</button>
-			</form>
-		);
-		
-	}
+	return (
+		<form onSubmit={handleSubmit(values => onSubmit(values))}>
+			<div className='input'>
+				<label htmlFor='username'>Username</label>
+				<Field component='input' disabled={submitting} name='username' type='text'/>
+			</div>
+			<div className='input'>
+				<label htmlFor='password'>Password</label>
+				<Field component='input' disabled={submitting} name='password' type='text'/>
+			</div>
+			<button disabled={disabled} type="submit">{buttonText}</button>
+		</form>
+	);
 }
